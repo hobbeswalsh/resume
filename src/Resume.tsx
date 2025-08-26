d116142845dca9c9237633c254cac0dd6c02379e
Robin Walsh <rob.walsh@gmail.com>
2025-08-26 06:04:06 -0700

    Fixing typos

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -12,33 +12,38 @@ const Resume = () => {
       <div className="font-semibold mb-1">Experience</div>
       <div className="ml-[5%]">
         <Jobs jobs={allJobs} />
-        <div className="my-2 text-xs italic">
-          Information about the following jobs (further in the past) will be
-          furnished upon request
+        <div className="mb-2 mt-8 text-xs">
+          Further information about employment prior to 2011 will be furnished
+          upon request
         </div>
         <div className="text-xs flex flex-col gap-1">
           <div>
             <span className="font-bold">Wordnik</span>, System
-            Administrator/Software developer - San Mateo, CA
+            Administrator/Software developer - San Mateo, CA{" "}
+            <span className="text-slate-500">2009-2011</span>
           </div>
           <div>
-            <span className="font-bold">Yahoo!</span>, DNS Guru - Sunnyvale, CA
+            <span className="font-bold">Yahoo!</span>, DNS Guru - Sunnyvale, CA{" "}
+            <span className="text-slate-500">2007-2009</span>
           </div>
           <div>
             <span className="font-bold">UnitedLayer</span>, System
-            administrator/software developer - San Francisco, CA
+            administrator/software developer - San Francisco, CA{" "}
+            <span className="text-slate-500">2006-2007</span>
           </div>
         </div>
       </div>
       <hr className="my-2 shadow-2xl text-slate-500" />
       <div className="font-semibold my-2">Skills/Languages</div>
-      <div>
+      <div className="ml-[5%]">
         Typescript/React, Java, Go, Python, SQL, Terraform, Shell, Linux/*BSD
       </div>
       <div className="font-semibold my-2">Education</div>
-      <div>University of Chicago, Honors in Language and Literature, 2003</div>
+      <div className="ml-[5%]">
+        University of Chicago, Honors in Language and Literature, 2003
+      </div>
       <div className="font-semibold my-2">Publications</div>
-      <div>
+      <div className="ml-[5%]">
         Two blog posts for OCI:{" "}
         <a
           className="text-blue-500"
--- src/job.ts
+++ src/job.ts
@@ -11,69 +11,69 @@ export interface Job {
 }
 
 const opsCentral: Job = {
-  title: "Principal Software developer",
+  title: "Principal Software Developer",
   company: "OCI",
   className: 'text-violet-500',
   team: "Ops Central",
   location: "Seattle, WA",
   description:
-    "Worked on the \"Ops Central\" team to standardize and unify all operations at OCI. Responsibilities included spearheading programs to get other teams to onboard to standardized observability and service catalog, as well as building out the UI elements they would eventually use, and run interviews them about common use cases to collect feedback.",
+    "Worked on the \"Ops Central\" team to standardize all operations at OCI. Responsibilities included: Leading a program to encourage service teams to onboard to OCI standardized observability, building out the UI elements for Ops Central, and interview users about common operational use cases to improve the UI.",
   startDate: new Date("2024-04-01T00:00:00-0800"),
   endDate: new Date("2025-08-15T00:00:00-0800"),
   accomplishments: [
     "Ran Oracle-wide program to migrate users from existing tools to Ops Central UIs",
-    "Contributed to basic framework of app, including deployment strategy, basic layout/look-and-feel/keyboard navigation",
-    "Contributed work for a handful of the fundamental tools, including Metrics and Logging",
+    "Interviewed engineers on other teams; identified and documented feature gaps",
+    "Contributed to basic framework of app, including deployment strategy, basic layout/look-and-feel/keyboard navigation, and core UIs",
   ],
 };
 
 const dope: Job = {
-  title: "Principal Software developer",
+  title: "Principal Software Developer",
   company: "OCI",
   className: 'text-emerald-500',
   team: "DevOps Portal Experience",
   location: "Seattle, WA",
   description:
-    "Built the DevOps Portal Experience from scratch with another developer, and then operated it with the team we grew. The portal (DOPE) is the center for all ops, deployments, and day-to-day management of all OCI infrastructure across dozens of regions.",
+    "Built the DevOps Portal Experience (DOPE) from scratch with another developer, and then expanded and operated it as OCI scaled from 2 to over 50 regions. DOPE is the central UI for all ops, deployments, and day-to-day management of OCI's global infrastructure.",
   startDate: new Date("2016-05-01T00:00:00-0800"),
   endDate: new Date("2024-04-01T00:00:00-0800"),
   accomplishments: [
     "Built the infrastructure and large parts of the UI from scratch",
-    "Built the canary to monitor DOPE's infrastucture and network connectivity",
-    "Architected and built most of the solution that allowed DOPE to scale seamlessly as OCI built dozens of new regions",
-    "Published two OCI blog posts explaining the canary we architected and built",
-    "Helped design the eventual federation of all webapps",
+    "Architected and built the network proxy that allowed DOPE to scale seamlessly as OCI built dozens of new regions",
+    "Built the canary to monitor DOPE's health and network connectivity",
+    "Published two OCI blog posts explaining the DOPE canary",
+    "Helped design federation so internal OCI developers could use our platform",
   ],
 };
 
 const counsyl: Job = {
-  title: "Senior Software developer",
+  title: "Senior Software Developer",
   className: 'text-orange-500',
   company: "Counsyl",
   team: "Platform",
   location: "San Francisco, CA",
   description:
-    "Managed all monitoring and infrastructure for a biotech startup",
+    "Managed monitoring, infrastructure, CI/CD, and tooling for a biotech startup",
   startDate: new Date("2014-09-01T00:00:00-0800"),
   endDate: new Date("2016-04-01T00:00:00-0800"),
   accomplishments: [
     "Implemented multiple control/data planes for the company: load balancing, monitoring, DNS, secret management, service discovery",
+    "Integrated some Hashicorp tools (Terraform, Vault, Consul) to automate deployments for all internal teams",
     "Modernized the puppet installation for the company",
-    "Integrated some Hashicorp tools (Terraform, Vault, Consul) to automate deployments and rollbacks across the company",
   ],
 };
 
 const amazon: Job = {
-  title: "Software developer (SDE2)",
+  title: "Software Developer (SDE2)",
   className: 'text-rose-500',
   company: "Amazon",
   team: "DNS/L7",
   location: "Seattle, WA",
-  description: "Managed all production DNS and Load Balancing infrastructure",
+  description: "Managed production DNS and Load Balancing infrastructure for Amazon",
   startDate: new Date("2011-01-01T00:00:00-0800"),
   endDate: new Date("2014-08-01T00:00:00-0800"),
   accomplishments: [
-    "Wrote and managed software to control the DNS and Load Balancer fleer at AWS and Amazon",
+    "Wrote and managed software to manage the DNS and Load Balancer fleet at AWS and Amazon",
     "Wrote and maintained services to enable DNS self-service for internal AWS customers",
     "Improved observability for the global DNS fleet",
   ],
